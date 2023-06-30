ab64efb4e05918aa15f3873fbc2c2742c071ec32
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-06-30 22:16:37 +0900

    エラー解消

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import questionController from "./controllers/questionController";
 import userController from "./controllers/userController";
 const app = express();
+app.use(express.json());
 app.use("/question", questionController);
 app.use("/user", userController);
 export default app;
